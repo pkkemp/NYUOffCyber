07953482fd003476cad73036a8e40ce19cc591d1
Preston <preston@prestonkemp.com>
2020-04-27 03:31:20 -0500

    crypto

---
+++ crypto/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	solvesbxor()
+}
+
+func solvesbxor() {
+	 var ciphertext []byte = {'60','6a','67','61','7d','44','74','73','72','35','59','60','36','74','65','63','59405251593237606232303465323437367b}
+	 print(ciphertext)
+}
---
+++ crypto/testxor.py
@@ -0,0 +1,2 @@
+import xortool
+def main:
